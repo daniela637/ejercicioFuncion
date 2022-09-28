fd106d7717bc49f076045605ba2a82af65222ee7
daniela637 <78282623+daniela637@users.noreply.github.com>
2022-09-28 16:51:34 -0300

    Add files via upload

---
+++ funciones/src/Coche.java
@@ -0,0 +1,14 @@
+public class Coche {
+
+
+
+    int puertas;
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas() {
+        this.puertas++;
+    }
+}
---
+++ funciones/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /////objeto---------------
+        Coche micoche = new Coche();
+        micoche.setPuertas();
+        micoche.setPuertas();
+        System.out.println("cantidad de puertas :" + micoche.getPuertas());
+
+
+
+
+int resultado;
+    resultado = suma(30,10,8);
+
+        System.out.println("resultado de la suma : " + resultado);
+    }
+
+    public static int suma (int a, int b, int c){
+
+       return a + b + c;
+
+    }
+}
